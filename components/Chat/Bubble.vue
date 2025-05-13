9da740c9196e53598392adf9d1f98346e58da230
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-13 12:02:27 +0300

    Refactor chat message rendering in Main component; replace static messages with ChatBubble component for improved maintainability

--- components/Chat/Bubble.vue
+++ components/Chat/Bubble.vue
@@ -6,17 +6,28 @@ const porps = defineProps<{
 const { username } = toRefs(useAuth().authState.value);
 </script>
 <template>
-  <div class="flex items-start gap-3">
+  <div
+    class="flex items-start gap-3"
+    :class="{
+      'flex-row-reverse': isUser,
+    }"
+  >
     <img
       v-if="!isUser"
       src="/assets/bruno.svg"
       class="h-10 w-10 rounded-full border-2 border-emerald-100"
     />
-
+    <div v-else class="h-10 w-10 rounded-full border-2 border-emerald-100">
+      {{ getUserNameInitials(username) }}
+    </div>
     <div
-      class="max-w-[80%] rounded-2xl bg-emerald-100 px-4 py-3 text-slate-800 shadow-sm"
+      class="max-w-[80%] rounded-2xl px-4 py-3 shadow-sm"
+      :class="{
+        'border-emerald-200 bg-white text-slate-800': isUser,
+        'bg-emerald-100 text-slate-800': !isUser,
+      }"
     >
-      Assalamu Alaikum! How can I assist you in your Islamic research today?
+      {{ message }}
       <div class="mt-1 text-right text-xs text-slate-500">11:15 AM</div>
     </div>
   </div>
--- components/Chat/Main.vue
+++ components/Chat/Main.vue
@@ -16,35 +16,15 @@
 
     <!-- Chat Messages -->
     <div class="flex max-h-96 flex-col gap-4 overflow-y-auto px-2">
-      <!-- Received Message -->
-      <div class="flex items-start gap-3">
-        <img
-          src="/assets/bruno.svg"
-          class="h-10 w-10 rounded-full border-2 border-emerald-100"
-        />
-        <div
-          class="max-w-[80%] rounded-2xl bg-emerald-100 px-4 py-3 text-slate-800 shadow-sm"
-        >
-          Assalamu Alaikum! How can I assist you in your Islamic research today?
-          <div class="mt-1 text-right text-xs text-slate-500">11:15 AM</div>
-        </div>
-      </div>
-
-      <!-- Sent Message -->
-      <div class="flex flex-row-reverse items-start gap-3">
-        <div
-          class="max-w-[80%] rounded-2xl border border-emerald-200 bg-white px-4 py-3 text-slate-800 shadow-sm"
-        >
-          Wa Alaikum Salaam. Can you explain the significance of the 5 daily
-          prayers?
-          <div class="mt-1 text-right text-xs text-slate-500">11:17 AM</div>
-        </div>
-        <div
-          class="flex h-10 w-10 items-center justify-center rounded-full bg-emerald-500 font-bold text-white shadow"
-        >
-          JS
-        </div>
-      </div>
+      <ChatBubble
+        :is-user="false"
+        message="Assalamu Alaikum! How can I assist you in your Islamic research today?"
+      />
+      <ChatBubble
+        :is-user="true"
+        message=" Wa Alaikum Salaam. Can you explain the significance of the 5 daily
+          prayers?"
+      />
     </div>
 
     <ChatMessageForm />
